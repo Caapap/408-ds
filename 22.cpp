2b6219413bbcffdeea22ed95e652209a06c14224
caapap <caapap@qq.com>
2022-09-24 02:49:52 -0700

    2022DS

---
+++ 22.cpp
@@ -0,0 +1,18 @@
+void inorder (TreeNode* root)
+    if(root==null) return;
+    inorder(root->left);
+    cout<<root->val;
+    inorder(root->right);
+
+    bool flag=true;
+    int k=0;
+    void inorder( Sqlist tree, int i) {
+        if(i>tree.size||tree.data[i]=-1||!falg) return;
+        inorder(tree, i*2+1);
+        if(k>tree.data[i*2+1]){
+            flag=false;
+            return;
+        }
+        k=tree.data[i*2+1];
+        inorder(tree, i*2+2);
+    }
